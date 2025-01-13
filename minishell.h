d8939e7be4ba2137ad8f34cc6ad7064231256655
lorey2 <lorey@student.42lausanne.ch>
2025-01-13 23:24:57 +0100

    Update minishell.h

--- minishell.h
+++ minishell.h
@@ -1,25 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lorey <lorey@student.42lausanne.ch>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/30 02:50:39 by lorey             #+#    #+#             */
-/*   Updated: 2024/12/30 02:53:20 by lorey            ###   LAUSANNE.ch       */
-/*                                                                            */
-/* ************************************************************************** */
+PLACEHOLDER
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/types.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <sys/wait.h>
-# include <limits.h>
-
-#endif
+EVERYTHING IS IN SRC
